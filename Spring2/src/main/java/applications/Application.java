1b2cb68a9204136252b404efc94f70e377190a71
kkassin <kkassin@edr18030.dip.tu-dresden.de>
2018-09-13 14:58:35 +0200

    Changed package structure of Spring2 project

--- Spring2/src/main/java/applications/Application.java
+++ Spring2/src/main/java/applications/Application.java
@@ -1,4 +1,4 @@
-package hello;
+package applications;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -20,5 +20,5 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-    }
+    }   
 }
