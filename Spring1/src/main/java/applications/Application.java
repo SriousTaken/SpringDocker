d250d1e381e837d0952025c54ecbb04edc372512
kkassin <kkassin@edr18030.dip.tu-dresden.de>
2018-09-14 11:54:19 +0200

    Created Index page for Spring1 web application
    Also cleaned and documented Artifacts of the Spring1 web application

--- Spring1/src/main/java/applications/Application.java
+++ Spring1/src/main/java/applications/Application.java
@@ -12,12 +12,16 @@ import org.springframework.web.client.RestTemplate;
 import literals.Literals;
 import util.CommandExecutioner;
 
+/**
+ * Defines a Spring web application using Rest calls
+ * @author Kevin Kassin
+ */
 @SpringBootApplication
 @RestController
 public class Application {
 	
 	/**
-	 * Literals of this application, for example, IDs, URI parts, ports...
+	 * Literals of this application, for example IDs, URI parts, ports...
 	 */
 	Literals literals = new Literals();
 	
@@ -28,38 +32,59 @@ public class Application {
 	
 	/**
 	 * Builds the list of URIs of communication partners
-	 * @return
+	 * @return list of URIs of communication partners
 	 */
 	public List<String> initialize_comm_partners() {
 		List<String> comm_partners = new ArrayList<String>();
 		comm_partners.add("http://" + literals.CLUSTER_IP+ ":" + literals.PORTS_COMM_PARTNERS.get("Spring2") + "/pong");
 		return comm_partners;
 	} 
-	
+
+	/**
+	 * Runs the Spring web application
+	 */
 	public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+	/**
+	 * Defines the index page of the Spring web application
+	 * @return
+	 */
 	@RequestMapping("/")
-    public String home() {
-        return "Hello Docker World TEST 1";
+    public String index() {
+        String uri = "http://" + literals.CLUSTER_IP+ ":" + literals.PORT;
+		return "<h1>Spring Application on Docker</h1>"
+        		+ "This is Spring Application " + literals.App_ID + "<br>"
+        		+ "<ul>" 
+        		+ "<li><a href=" + uri + "/ping>Ping Pong Scenario</a></li>" 
+        		+ "<li><a href=" + uri + "/alive>Alive Checking Scenario</a></li>"
+        		+ "<li>Command Scenario <ul>"
+        			+ "<li><a href=" + uri + "/command_fix>Fix command: ls</a></li>"
+        			+ "<li><a href=\" + uri + \"/command_input>Command input</a></li>"
+        			+ "</ul>"
+        		+"<ul>";
     }
-   
+  
+	/**
+	 * Pings to the Spring web application Spring2 to get a pong answer
+	 * @return String representing the answer of Spring web application Spring2
+	 */
     @RequestMapping("/ping")
     public String ping() {
+    	String pingPongPartner = "http://" + literals.CLUSTER_IP+ ":" + literals.PORTS_COMM_PARTNERS.get("Spring2") + "/pong";
     	RestTemplate restTemplate = new RestTemplate();
-    	String result = "";
-    	for(String comm_partner : COMM_PARTNERS) {
-    		result = restTemplate.getForObject(comm_partner, String.class);
-    	}	
+    	String result = "Spring2 says: " + restTemplate.getForObject(pingPongPartner, String.class);	
     	return result;
     }
     
-    @RequestMapping("/command")
+    /**
+     * Executes a ls command on the docker the Spring web application is running on
+     * @return String representing the result of the ls command
+     */
+    @RequestMapping("/command_fix")
     public String command() {
     	CommandExecutioner commandExecutioner = new CommandExecutioner("ls", System.getProperty("user.home"));
     	return commandExecutioner.execute();
     }
-
-    
 }
--- Spring1/src/main/java/literals/Literals.java
+++ Spring1/src/main/java/literals/Literals.java
@@ -3,21 +3,41 @@ package literals;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Application literals, for example IDs, URI parts, ports...
+ * @author Kevin Kassin
+ */
 public class Literals {
 	
+	/**
+	 * The identifier of this Spring web application
+	 */
 	public final String App_ID = "Spring1";
 
+	/**
+	 * The IP of the cluster the docker running this Spring web application
+	 */
 	public final String CLUSTER_IP = "192.168.99.100";
 	
+	/**
+	 * The port of the docker running this Spring web application
+	 */
 	public final String PORT = "30125";
 	
+	/**
+	 * The ports of other dockers running Spring web applications to communicate to, mapped by their identifiers
+	 */
 	public final Map<String, String> PORTS_COMM_PARTNERS = initialize_ports_comm_partners();
 	
+	/**
+	 * Initializes the map of ports of other dockers running Spring web applications to communicate to
+	 * @return the map of ports of other dockers running Spring web applications to communicate to
+	 */
 	private Map<String, String> initialize_ports_comm_partners() {
 		Map<String, String> port_comm_partners = new HashMap<String, String>();
-		//begin set values
+		//begin: set values
 		port_comm_partners.put("Spring2", "30126");
-		//end set values
+		//end: set values
 		return port_comm_partners;
 	}
 	
